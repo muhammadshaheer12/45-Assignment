d1e96b1b811607023147b214c669b0b1205479ff
MuhammadShaheer12 <159147097+MuhammadShaheer12@users.noreply.github.com>
2024-04-19 02:29:18 +0500

    typescript

--- 13 assignment/main.ts
+++ 13 assignment/main.ts
@@ -1,4 +1,4 @@
-let car: string[] = ["honda", "changan", "audi", "toyota", "bugatti"];
+let car: string[] =["honda", "changan", "audi", "toyota", "bugatti"];
 console.log("i would like to own a " + car[0]);
 console.log("i would like to own a " + car[1]);
 console.log("i would like to own a " + car[2]);
