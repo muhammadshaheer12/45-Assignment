9905c4de4bb40b0dc1e08438c6cdb7c71d7bf1ce
MuhammadShaheer12 <159147097+MuhammadShaheer12@users.noreply.github.com>
2024-04-19 00:14:32 +0500

    typescript

--- 12 assignment/main.ts
+++ 12 assignment/main.ts
@@ -1,4 +1,4 @@
-let names : string[] = ["rohab", "rafay", "hamza", "bilal", "ahsan"];
+let names: string[] = ["rohab", "rafay", "hamza", "bilal", "ahsan"];
 console.log(names[0] + " i just wanted to say you are doing amazing keep pushing towards your goals");
 console.log(names[1] + " i just wanted to say you are doing amazing keep pushing towards your goals");
 console.log(names[2] + " i just wanted to say you are doing amazing keep pushing towards your goals");
